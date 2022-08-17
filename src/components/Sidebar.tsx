0036d6e212ce41b40eb0f5eb5acb6ed8442ef223
Tushar Ojha <tusharojha2001@gmail.com>
2022-08-18 02:37:33 +0530

    added sidebar from json

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './sidebar.css';
+
+import data from '../data.json';
+
+const Item = ({ title, selected, onTap }: { title: string, selected: boolean, onTap: () => void }) => {
+  return <div onClick={onTap} className={'side-item' + (selected ? ' selected' : '')}>
+    {title}
+  </div>
+}
 
 const Sidebar = () => {
-  return <div></div>
+  const [selectedKey, setSelectedKey] = useState('')
+
+  useEffect(() => {
+    const firstKey = Object.keys(data).at(0) ?? ''
+    setSelectedKey(firstKey + (data as any)[firstKey][0].key)
+  }, [])
+
+  return <div>
+    <div className='side-container'>
+      {Object.keys(data).map((key) => {
+        return <div className='sidebar-section'>
+          <h6>{key}</h6>
+          {((data as any)[key]).map((item: any) => {
+            return <Item title={item.key} onTap={() => setSelectedKey(key + item.key)} selected={selectedKey == key + item.key} />
+          })}
+        </div>
+      })}
+    </div>
+  </div>
 }
 
 export default Sidebar
