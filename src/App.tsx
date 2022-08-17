e16e75fda59b90d469f53eb720aa961150ac0ec6
Tushar Ojha <tusharojha2001@gmail.com>
2022-08-18 01:29:25 +0530

    added logo section

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 import runPlayground from './playground';
 import "antd/dist/antd.css";
 import { Button, Row, Col } from 'antd'
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   const [response, setResponse] = useState<any>({})
   const [loading, setLoading] = useState<boolean>(false)
+  const [snippet, setSnippet] = useState('')
 
   const onClickHandler = async () => {
     setLoading(true)
@@ -18,20 +20,27 @@ function App() {
     setLoading(false)
   }
 
+  const Logo = () => {
+    return <div className='header-section'>
+      <img className='logo' src="./subsocial.png" /><span className='logo-title'> playground</span>
+    </div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Subsocial Playground</h2>
-        <Row className='items'>
-          <Col className='itemblock' span={10}>
-            <p>Test your code for subsocial</p>
-            <Button size='large' type='primary' disabled={loading} onClick={() => onClickHandler()}>{loading ? 'Loading...' : 'Run'}</Button>
-          </Col>
-          <Col className='itemblock' span={10}>
-            <ReactJson style={{ fontSize: 20 }} collapsed={false} src={response} />
-          </Col>
-        </Row>
-      </header>
+      <Logo />
+      <Row className='items'>
+        <Col className='itemblock' span={5}>
+          <Sidebar />
+        </Col>
+        <Col className='itemblock' span={10}>
+          <p>Test your code for subsocial</p>
+          <Button size='large' type='primary' disabled={loading} onClick={() => onClickHandler()}>{loading ? 'Loading...' : 'Run'}</Button>
+        </Col>
+        <Col className='itemblock' span={8}>
+          <ReactJson style={{ fontSize: 20 }} collapsed={false} src={response} />
+        </Col>
+      </Row>
     </div>
   );
 }
