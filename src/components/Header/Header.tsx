35b65d171e541efda29c252d5b7a97eaf66d82cf
Tushar Ojha <tusharojha2001@gmail.com>
2022-10-13 11:33:03 +0530

    added header details

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material"
 
 import Image from 'next/image';
 import MenuIcon from '@mui/icons-material/MenuOutlined';
+import Head from 'next/head';
 import { useAppSelector } from "../../redux/hooks";
 
 const LEARN_MORE_LINK = "https://docs.subsocial.network/docs/develop"
@@ -15,6 +16,16 @@ const Header = (props: HeaderProps) => {
 
   const selectedItem = useAppSelector((state) => state.code.selectedItem)
   return <AppBar color="secondary" position="static" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+    <Head>
+      <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="Play with Subsocial's JS SDK in the playground."
+      />
+      <title>Subsocial Playground</title>
+    </Head>
     <Toolbar>
       <IconButton
         onClick={toggleDrawer}
